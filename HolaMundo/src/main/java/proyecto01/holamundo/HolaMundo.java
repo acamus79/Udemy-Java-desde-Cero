e5702f24cec98510f7d6e4bba77368e22f022fb1
acamus79 <adriancamus1979@gmail.com>
2021-08-10 23:22:01 -0300

    Udemy Java desde Cero Ejercicio 01 Hola Mundo

---
+++ HolaMundo/src/main/java/proyecto01/holamundo/HolaMundo.java
@@ -0,0 +1,18 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+package proyecto01.holamundo;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class HolaMundo {
+      
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Otra linea de codigo");
+    }
+}
