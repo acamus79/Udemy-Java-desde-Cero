90a957042fec2be6f21b9db0e99e214533d28300
acamus79 <adriancamus1979@gmail.com>
2021-08-11 00:20:21 -0300

    Udemy Java desde Cero Ejercicio 02

---
+++ 03_SalidaEstandar2/src/main/java/principal/MostrandoTexto.java
@@ -0,0 +1,17 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+package principal;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class MostrandoTexto {
+    public static void main(String[] args) {
+        System.out.print("Un texto cualquiera. ");
+        System.out.println("Otro texto");
+    }
+}
