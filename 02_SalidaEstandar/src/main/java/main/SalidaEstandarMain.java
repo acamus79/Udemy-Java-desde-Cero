1ea4a1a9b8b2561b42e3d5fa815adf7eeaee2259
acamus79 <adriancamus1979@gmail.com>
2021-08-10 23:53:41 -0300

    Udemy Java desde Cero Ejercicio 02

---
+++ 02_SalidaEstandar/src/main/java/main/SalidaEstandarMain.java
@@ -0,0 +1,25 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package main;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class SalidaEstandarMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+        
+        System.out.println("Esta es la salida de mi programa");
+        
+    }
+
+}
